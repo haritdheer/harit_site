c6c75d7372e83db8361128266553a2d2cff8a5b7
kamalbhaiii <kamal5201ks@gmail.com>
2024-08-31 22:22:56 +0200

    Contact DEU support

--- src/components/about_page/index.jsx
+++ src/components/about_page/index.jsx
@@ -3,10 +3,12 @@ import PageHeader from "../page_header";
 import CustomContainer from "../../containers/customContainer";
 import AboutPageLeft from "./about_page_left";
 import AboutPageRight from "./about_page_right";
+import { useTranslation } from "react-i18next";
 
 const Aboutpage = () => {
+  const { t } = useTranslation();
   return (
-    <PageHeader heading={"About Me"} headingId="01">
+    <PageHeader heading={t("navbarCompo1")} headingId="01">
       <CustomContainer className={"flex"}>
         <AboutPageLeft />
         {/* <AboutPageRight /> */}
--- src/components/contact_page/index.jsx
+++ src/components/contact_page/index.jsx
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import PageHeader from "../page_header";
 import CustomContainer from "../../containers/customContainer";
 import CustomButtonAnchor from "../custom_button/customButtonAnchor";
+import { useTranslation } from "react-i18next";
 
 const Contactpage = () => {
   const [mail, setMail] = useState("coding.coding.everyday@gmail.com");
+  const { t } = useTranslation();
   return (
     <PageHeader
-      heading="Hack to reach me!"
+      heading={t("contactPageHeading")}
       headingId="04"
       className={"h-full"}
       childrenClass={"w-full h-full"}
@@ -17,13 +19,16 @@ const Contactpage = () => {
           "h-full flex flex-col items-center justify-center gap-y-5 md:gap-y-10"
         }
       >
-        <h3 className="text-3xl md:text-6xl font-bold">Get In Touch</h3>
+        <h3 className="text-3xl md:text-6xl font-bold">
+          {t("contactPageSubHeading")}
+        </h3>
         <p className="w-full md:w-1/2 text-center text-md">
-          Although I’m not currently looking for any new opportunities, my inbox
-          is always open. Whether you have a question or just want to say hi,
-          I’ll try my best to get back to you!
+          {t("contactPageContent")}
         </p>
-        <CustomButtonAnchor href={`mailto:${mail}`} title={"Say Hello!"} />
+        <CustomButtonAnchor
+          href={`mailto:${mail}`}
+          title={t("contactPageBtn1")}
+        />
       </CustomContainer>
     </PageHeader>
   );
